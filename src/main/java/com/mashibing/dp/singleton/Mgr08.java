dc1174b8a73db1d3546b8d155e7ebb54f4374506
anught <782655752@qq.com>
2020-07-21 22:21:41 +0800

    first

--- src/main/java/com/mashibing/dp/singleton/Mgr08.java
+++ src/main/java/com/mashibing/dp/singleton/Mgr08.java
@@ -5,16 +5,17 @@ package com.mashibing.dp.singleton;
  */
 public enum Mgr08 {
 
-    INSTANCE;
+	INSTANCE;
 
-    public void m() {}
+	public void m() {
+	}
 
-    public static void main(String[] args) {
-        for(int i=0; i<100; i++) {
-            new Thread(()->{
-                System.out.println(Mgr08.INSTANCE.hashCode());
-            }).start();
-        }
-    }
+	public static void main(String[] args) {
+		for (int i = 0; i < 100; i++) {
+			new Thread(() -> {
+				System.out.println(Mgr08.INSTANCE.hashCode());
+			}).start();
+		}
+	}
 
 }
